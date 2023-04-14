597e46f6adb61089bfa573bb5e98b01181789719
0xKarasu <0xKarasu@protonmail.com>
2023-04-14 22:18:37 +0900

    :rocket: init frontend repo

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
+module.exports = {
+  content: [
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.js",
+    "./public/**/*.html",
+    "./components/**/*.{ts,tsx}",
+    "./pages/**/*.{ts,tsx}",
+  ],
+  plugins: [require("flowbite/plugin")],
+  theme: {
+    fontFamily: {
+      heading: ["Rajdhani", "sans-serif"],
+      body: ["Poppins", "sans-serif"],
+    },
+  },
+};

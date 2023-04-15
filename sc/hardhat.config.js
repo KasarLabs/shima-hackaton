14c6e3cd0b4bd5e33c8fcec8f14970176f066a46
HxSimo <HxSimo@proton.me>
2023-04-16 07:43:20 +0900

    hardhat config

--- sc/hardhat.config.js
+++ sc/hardhat.config.js
@@ -3,5 +3,15 @@ require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+    networks: {
+        hardhat: {},
+        celo: {
+          url: "https://forno.celo.org", // Replace with your own node endpoint
+          gasPrice: 10e9,
+          gas: 8000000,
+          accounts: [process.env.PRIVATE],
+          chainId: 42220, // Replace with the chainId of the Celo network you want to use
+        },
+    },
+    solidity: "0.8.18",
 };
